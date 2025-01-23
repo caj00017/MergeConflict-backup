27a07c86d5f5ddceec0dfb43fa3cdf29dcb4be2c
Tanner <tannerf0526@gmail.com>
2025-01-23 13:44:02 -0500

    polling function updated with support for all characters and return

--- kernel/serial.c
+++ kernel/serial.c
@@ -1,6 +1,7 @@
 #include <mpx/io.h>
 #include <mpx/serial.h>
 #include <sys_req.h>
+#include <stdlib.h>
 
 enum uart_registers {
 	RBR = 0,	// Receive Buffer
@@ -62,10 +63,20 @@ int serial_out(device dev, const char *buffer, size_t len)
 
 int serial_poll(device dev, char *buffer, size_t len)
 {
-	while(sizeof(buffer) < len){
-		if(inb(dev + LSR) == 1){
-			char c = inb(dev);
-			int ASCIIc = atoi(c);
+	int pos = 0;
+	int exit_return = 0;
+
+	while(sizeof(buffer) < len || exit_return == 1) /* looping while the buffer size is less than the total length */ {
+		if(inb(dev + LSR) & 1) /* checks to see if their is a byte to read */ {
+			char c = inb(dev); //reads the byte using inb
+			const char *c_ptr = &(c);
+			if(c == '\n'){  //checks for escape sqeuence and then exits
+				exit_return = 1;
+			}
+			if(atoi(c_ptr) >= 32 || atoi(c_ptr) <= 126){ 
+				buffer[pos] = c;  //adds the char to the buffer
+				pos++;  //updates position in buffer
+			}
 		}
 	}
 
