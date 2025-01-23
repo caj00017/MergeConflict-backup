2b29377cd07d0dd3a5e677848e7c4d4035d20681
caj00017 <caj00017@mix.wvu.edu>
2025-01-23 13:30:55 -0500

    shutdown logic

--- user/comexec.c
+++ user/comexec.c
@@ -1,6 +1,12 @@
 // Include required header files
 #include "mpx/comexec.h";
 
-void comexec(void) {
+int comexec(char buf[]) {
     // Code for executing commands
+
+    // IF the command is to shutdown, return a status code indicating shutdown.
+    if (buf == "shutdown") {
+        return 1;
+    }
+    
 }
