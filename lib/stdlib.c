b26215c1193a6d957a3ba739d0e0274b55bc3b1d
TunarForbes <tannerf0526@gmail.com>
2025-01-29 14:20:02 -0500

    changes to stdlib.c

--- lib/stdlib.c
+++ lib/stdlib.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <ctype.h>
-#include <stdbool.h>
 
 int atoi(const char *s)
 {
@@ -31,7 +30,6 @@ int atoi(const char *s)
 
 char* itoa(int num, char* str, int base){
 	int i = 0;
-	bool Negative = false;
 
 	if(num == 0){
 		str[i++] = '0';
@@ -43,10 +41,6 @@ char* itoa(int num, char* str, int base){
 		int rem = num % base;
 		str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
 		num = num / base;
-	}
-
-	if(Negative){
-		str[i++] = '-';
 	}
 
 	str[i] = '\0';
--- user/comexec.c
+++ user/comexec.c
@@ -8,39 +8,32 @@
 #include <mpx/interrupts.h>
 #include <stdlib.h>
 
-// Implementation for comexec
+char bcdToChar(unsigned char bcd);
+
 int comexec(char buf[]) {
+    // Code for executing commands
 
-    // "shutdown" logic
+    // Cue giant wall of if statements. There has to be a better way to do this.
     if (strcmp(buf, "shutdown") == 0) {
-
-        // return 1 indicating comhand loop break
         return 1;
     }
-
-    // "version" logic
-    else if (strcmp(buf, "version") == 0) {
-
-        // Write the current version to the console
+    if (strcmp(buf, "version") == 0) {
+        // "error: implicit declaration of function 'puts' is invalid in C99"
+        // another function must be used
         char msg[100] = "\nVersion 1.0\nCompilation Date: 1/27/2025";
         sys_req(WRITE,COM1, msg, sizeof(msg));
 
-        // return 0 indicating successful command execution
+        // serial_out(COM1, test, 8); 
         return 0;
     }
-<<<<<<< HEAD
     if (strcmp(buf, "get_date") == 0) {
 
         //disabling the NMI bit
         outb(0x70, inb(0x70) | 0x80);  
-=======
-
-    // "get_date" logic
-    else if (strcmp(buf, "get_date") == 0) {
-        outb(0x70, inb(0x70) | 0x80);
->>>>>>> fb208e955a3b50e1a72d732f0c0e99dcaad621d4
         char str[100];
         char *date_ptr;
+
+        //creating a slash char to put between the months/days/years
         char slash[1];
             slash[0] = '/';
         sys_req(WRITE, COM1, "\n", sizeof("\n"));
@@ -76,9 +69,7 @@ int comexec(char buf[]) {
         
         return 0;
     }
-
-    // "set_date" logic - not yet implemented
-    else if (strcmp(buf, "set_date") == 0) {
+    if (strcmp(buf, "set_date") == 0) {
         cli();
         outb(0x70, 0x08);
         outb(0x70, 0x06);
@@ -86,7 +77,6 @@ int comexec(char buf[]) {
         // printf("Date set to %s", buf);
         return 0;
     }
-<<<<<<< HEAD
     if (strcmp(buf, "get_time") == 0) {
 
         //disabling the NMI bit
@@ -121,52 +111,55 @@ int comexec(char buf[]) {
             }
             time_ptr = (itoa(seconds, str, 10));
             sys_req(WRITE, COM1, time_ptr, sizeof(time_ptr));
-=======
-
-    // "get_time" logic - not yet implemented
-    else if (strcmp(buf, "get_time") == 0) {
->>>>>>> fb208e955a3b50e1a72d732f0c0e99dcaad621d4
         // printf("%s", get_time());
         return 0;
     }
+    if (strcmp(buf, "set_time") == 0) {
+        
+        
 
-    // "set_time" logic - not yet implemented 
-    else if (strcmp(buf, "set_time") == 0) {
         // printf("Time set to %s", buf);
         return 0;
     }
-
-    // "help" logic
-    else if (strcmp(buf, "help") == 0) {
-
-        // Print list of each command
+    if (strcmp(buf, "help") == 0) {
+        // Print list of commands
         sys_req(WRITE, COM1, "\n@ help\t\tPrints a complete list of each available command.", sizeof("@ help\tPrints a complete list of each available command."));
         sys_req(WRITE, COM1, "\n@ version\tPrints the current version of the program.", sizeof("@ version\tPrints the current version of the program."));
         sys_req(WRITE, COM1, "\n@ get_date\tPrints the current date set by the user.", sizeof("@ get_date\tPrints the current date set by the user."));
         sys_req(WRITE, COM1, "\n@ set_date\tSets the current date.", sizeof("@ set_date\tSets the current date."));
         sys_req(WRITE, COM1, "\n@ get_time\tPrints the current time set by the user.", sizeof("@ get_time\tPrints the current time set by the user."));
         sys_req(WRITE, COM1, "\n@ set_time\tSets the current time.", sizeof("@ set_time\tSets the current time."));
-        
-        // return 0 indicating success
         return 0;
     } 
-
-    // logic for invalid commands
     else {
-
-        // print error message
         sys_req(WRITE, COM1, "\nError: Invalid command.", sizeof("\nError: Invalid command."));
-
-        // return 2 indicating failure
         return 2;
     }
 
-    // Unreachable code. comexec.c will not compile without this line.
+    // Invalid command. There may be different logic for this in the future.
     return -1;
 }
 
-// Implementation for bcdToChar
 char bcdToChar(unsigned char bcd){
     int decimal = (bcd >> 4) * 10 + (bcd & 0x0F);
     return decimal + '0';
 }
+
+// The following functions await implementation.
+// comexec.c does not compile without partial implementation of these functions.
+
+// char* get_date(void) {
+//     // Get date
+// }
+
+// void set_date(char* date) {
+//     // Set date
+// }
+
+// char* get_time(void) {
+//     // Get time
+// }
+
+// void set_time(char* time) {
+//     // Set time
+// }
