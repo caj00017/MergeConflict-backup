ae7b41f333877dcbe73afdf127cfb32a82eba1a7
caj00017 <caj00017@mix.wvu.edu>
2025-02-04 13:21:18 -0500

    functions for each command + commenting

--- include/mpx/comexec.h
+++ include/mpx/comexec.h
@@ -7,19 +7,63 @@
 #ifndef COMEXEC
 #define COMEXEC
 
-/*
-*   @brief Function for executing commands. Called by comhand().
-*   @param buf Buffer containig the current command to be executed.
-*   @return Status code indicating shutdown (1), successful command execution (0), or an invalid command (2).
-*   @authors Tanner Forbes, Chris Jones, Izaak Whetsell, Evan Humphrey
+/**
+* @brief Function for executing commands. Called by comhand().
+* @param buf Buffer containig the current command to be executed.
+* @return Status code indicating shutdown (1), successful command execution (0), or an invalid command (2).
+* @authors Tanner Forbes, Chris Jones, Izaak Whetsell, Evan Humphrey
 */
 int comexec(char buf[]);
 
-/*
-*   @brief Function which utilizes the real time clock registers to display the current time. 
-*   @return Status code
-*   @author Tanner Forbes
-*/
-int get_time();
+/**
+ * @brief Terminates the command handling sequence.
+ * @return Status code (1 = shutdown, 0 = remain open).
+ * @author Izaak Whetsell, Chris Jones
+ */
+int shutdown(void);
+
+/**
+ * @brief Prints a list of each available command.
+ * @return Status code
+ * @author Chris Jones
+ */
+int help(void);
+
+/**
+ * @brief Prints the current version.
+ * @return Status code
+ * @author Chris Jones
+ */
+int version(void);
+
+/**
+ * @brief Function which utilizes the real time clock registers to display the current time. 
+ * @return Status code
+ * @author Tanner Forbes
+ */
+int get_time(void);
+
+/**
+ * @brief Function which utilizes the real time clock registers to set the current time. 
+ * @return Status code
+ * @param buf Command string to extract the time from
+ * @author Chris Jones
+ */
+int set_time(char buf[]);
+
+/**
+ * @brief Function which utilizes the real time clock registers to get the current date. 
+ * @return Status code
+ * @author Tanner Forbes
+ */
+int get_date(void);
+
+/**
+ * @brief Function which utilizes the RTC registers to set the current date. 
+ * @param buf Command string to extract the date from
+ * @return Status code
+ * @author Tanner Forbes
+ */
+int set_date(char buf[]);
 
 #endif
--- user/comexec.c
+++ user/comexec.c
@@ -10,77 +10,273 @@
 #include <ctype.h>
 #include <memory.h>
 
-char bcdToChar(unsigned char bcd);
-int get_time();
-int get_date();
-
 int comexec(char buf[]) {
     
-    // "shutdown" logic
     if (strcmp(buf, "shutdown") == 0 || strcmp(buf, "sd") == 0) {
-        //Write a newline to command line.
-        sys_req(WRITE, COM1, "\n", sizeof("\n"));
-
-        //Create variable for a one time only command per shutdown call
-        int first = 1;
-
-        //Loop as long as the person keeps mispelling / failed entry
-        while (1)
-        {
-            //Character response for double check
-            char response[100] = { 0 };
-
-            //For first time, write instructions
-            if(first== 1){
-                first++;
-                sys_req(WRITE, COM1, "@ Are you sure you want to shutdown? (y or n)", sizeof("@ Are you sure you want to shutdown? (y or n)"));
-            }
-
-            //Write formatting for entry and read from the command line
-            sys_req(WRITE, COM1, "\n@ ", sizeof("@ "));
-            sys_req(READ, COM1, response, sizeof(response));
-
-            //Check to see if the person want to shutdown (y) or continue running program(n)
-            if(strcmp(response, "y") == 0 || strcmp(response, "yes") == 0 ){
-                return 1; //exit and end the program
-            }
-            else if(strcmp(response, "n") == 0 || strcmp(response, "no") == 0 ){
-                return 0; //exit and continue running program
-            }
-            else{
-                //If they don't response with the proper entry, give repeated instructions and loop again.
-                sys_req(WRITE, COM1, "\n", sizeof("\n"));
-                sys_req(WRITE, COM1, "@ Please retype your response: (y or n)", sizeof("@ Please retype your response: (y or n)"));
-                continue;
-            }
-        }
-        
-        
-        return 1;
+        int status = shutdown();
+        return status;
     }
     if (strcmp(buf, "version") == 0 || strcmp(buf, "v") == 0) {
-        // "error: implicit declaration of function 'puts' is invalid in C99"
-        // another function must be used
-        char msg[100] = "\nVersion 1.0\nCompilation Date: 1/27/2025";
-        sys_req(WRITE,COM1, msg, sizeof(msg));
-
-        // serial_out(COM1, test, 8); 
+        version();
         return 0;
     }
-
-    // "get_date" logic
     else if (strcmp(buf, "get_date") == 0 || strcmp(buf, "gd") == 0) {
         get_date();
         return 0;
     }
-
     else if (strcmp(buf, "set_date") == 0) {
         sys_req(WRITE, COM1, "\nPlease enter the date to be set (MM/DD/YY): ", sizeof("\nPlease enter the date to be set (MM/DD/YY): "));
     }
-
-    // "set_date" logic - not yet implemented (IP: Tanner Forbes)
     else if (contains(buf, "set_date") == 1) {
+        set_date(buf);
+        return 0;
+    }
+    else if (strcmp(buf, "get_time") == 0 || strcmp(buf, "gt") == 0) {
+        get_time();
+        return 0;
+    }
+    else if (strcmp(buf, "set_time") == 0) {
+        sys_req(WRITE, COM1, "\nPlease enter the time to be set (HH:MM): ", sizeof("\nPlease enter the time to be set (HH:MM): "));
+    }
+    else if (contains(buf, "set_time") == 1) {
+        set_time(buf);
+        return 0;
+    }
+    if (strcmp(buf, "help") == 0) {
+        help();
+        return 0;
+    } 
+    else {
+        sys_req(WRITE, COM1, "\nError: Invalid command.", sizeof("\nError: Invalid command."));
+        return -1;
+    }
+}
 
+int shutdown(void) {
+ //Write a newline to command line.
+    sys_req(WRITE, COM1, "\n", sizeof("\n"));
+
+    //Create variable for a one time only command per shutdown call
+    int first = 1;
+
+    //Loop as long as the person keeps mispelling / failed entry
+    while (1)
+    {
+        //Character response for double check
+        char response[100] = { 0 };
+
+        //For first time, write instructions
+        if(first== 1){
+            first++;
+            sys_req(WRITE, COM1, "@ Are you sure you want to shutdown? (y or n)", sizeof("@ Are you sure you want to shutdown? (y or n)"));
+        }
+
+        //Write formatting for entry and read from the command line
+        sys_req(WRITE, COM1, "\n@ ", sizeof("@ "));
+        sys_req(READ, COM1, response, sizeof(response));
+
+        //Check to see if the person want to shutdown (y) or continue running program(n)
+        if(strcmp(response, "y") == 0 || strcmp(response, "yes") == 0 ){
+            return 1; //exit and end the program
+        }
+        else if(strcmp(response, "n") == 0 || strcmp(response, "no") == 0 ){
+            return 0; //exit and continue running program
+        }
+        else{
+            //If they don't response with the proper entry, give repeated instructions and loop again.
+            sys_req(WRITE, COM1, "\n", sizeof("\n"));
+            sys_req(WRITE, COM1, "@ Please retype your response: (y or n)", sizeof("@ Please retype your response: (y or n)"));
+            continue;
+        }
+    
+        return 0;
+    }
+}
+
+int help(void) {
+    // Print list of commands
+    sys_req(WRITE, COM1, "\n@ help\t\tPrints a complete list of each available command.", sizeof("@ help\tPrints a complete list of each available command."));
+    sys_req(WRITE, COM1, "\n@ version\tPrints the current version of the program.", sizeof("@ version\tPrints the current version of the program."));
+    sys_req(WRITE, COM1, "\n@ get_date\tPrints the current date set by the user.", sizeof("@ get_date\tPrints the current date set by the user."));
+    sys_req(WRITE, COM1, "\n@ set_date\tSets the current date.", sizeof("@ set_date\tSets the current date."));
+    sys_req(WRITE, COM1, "\n@ get_time\tPrints the current time set by the user.", sizeof("@ get_time\tPrints the current time set by the user."));
+    sys_req(WRITE, COM1, "\n@ set_time\tSets the current time.", sizeof("@ set_time\tSets the current time."));
+    sys_req(WRITE, COM1, "\n@ shutdown\tExits the program.", sizeof("\n@ shutdown\tExits the program."));
+    return 0;
+}
+
+int version(void) {
+    char msg[100] = "\nVersion 1.0\nCompilation Date: 1/27/2025";
+    sys_req(WRITE,COM1, msg, sizeof(msg));
+    return 0;
+}
+
+int get_time(void) {
+    //disabling the NMI bit
+        outb(0x70, inb(0x70) | 0x80);
+        char str[100];
+        char *time_ptr;
+        char colon[1];
+        colon[0] = ':';
+        sys_req(WRITE, COM1, "\n", sizeof("\n"));
+
+        //accessing the hour bit
+        outb(0x70, 0x04);
+
+            //changing the hour bit from bcd to a char ptr
+            time_ptr = (itoa(bcdToChar(inb(0x71)) - 53, str, 10)); // -5 offset
+
+            //writing the hour bit and a colon to the terminal
+            sys_req(WRITE, COM1, time_ptr, sizeof(time_ptr));
+            sys_req(WRITE, COM1, colon, sizeof(colon));
+
+        // accessing the minute bit
+        outb(0x70, 0x02);
+
+            //changing the minute bit from bcd to a char ptr
+            time_ptr = (itoa(bcdToChar(inb(0x71)) - 48, str, 10));
+
+            // append 0 to avoid (HH:M:SS) error
+            if (atoi(time_ptr) <= 9) {
+                sys_req(WRITE, COM1, "0", sizeof("0"));
+            }
+
+            sys_req(WRITE, COM1, time_ptr, sizeof(time_ptr));
+            sys_req(WRITE, COM1, colon, sizeof(colon));
+
+        // accessing the second bit 
+        outb(0x70, 0x00);
+
+            //changing the second bit from bcd to a char ptr
+            int seconds = bcdToChar(inb(0x71)) - 48;
+            if(seconds < 10){
+                sys_req(WRITE, COM1, "0", sizeof("0"));
+            }
+            time_ptr = (itoa(seconds, str, 10));
+            sys_req(WRITE, COM1, time_ptr, sizeof(time_ptr));
+
+        // return 0 indicating success
+        return 0;
+}
+
+int set_time(char buf[]) {
+    
+       // disable interrupts
+        cli();
+
+        // derive time string from the buffer
+        char* time = substr(buf, 9);
+
+        // check for invalid format (invalid time check pending)
+        if (charCount(time, ':') != 2 || strlen(time) != 8 || isNumeric(strtok(time, ":")) == 0) {
+            sys_req(WRITE, COM1, "\nInvalid time or format. (HH:MM:SS)", sizeof("\nInvalid time or format. (HH:MM:SS)"));
+            return -1;
+        }
+        else {
+            char hr_chars[3] = {time[0], time[1], '\0'}; // first 2 chars = hour value
+            char min_chars[3] = {time[3], time[4], '\0'}; // next 2 chars (skipping ':') = minute value
+            char sec_chars[3] = {time[6], time[7], '\0'}; // last 2 chars = second value
+
+            // check for invalid times / characters 
+            if (strcmp(hr_chars, "23") > 0 || strcmp(hr_chars, "0") < 0   
+            || strcmp(min_chars, "59") > 0 || strcmp(min_chars, "0") < 0  
+            || strcmp(sec_chars, "59") > 0 || strcmp(sec_chars, "0") < 0) 
+            {
+                sys_req(WRITE, COM1, "\nInvalid time or format. (HH:MM:SS)", sizeof("\nInvalid time or format. (HH:MM:SS)"));
+                return -1;
+            }
+
+            // retrieve int values for hour, minute, and second
+            unsigned int hour = (unsigned int)atoi(hr_chars) + 5; // +5 offset
+            unsigned int minute = (unsigned int)atoi(min_chars);
+            unsigned int second = (unsigned int)atoi(sec_chars);
+
+            // convert each time value to BCD
+            unsigned int hour_BCD = intToBCD(hour);
+            unsigned int minute_BCD = intToBCD(minute);
+            unsigned int second_BCD = intToBCD(second);
+
+            // write each BCD value to the corresponding register
+            outb(0x70, 0x04);
+            outb(0x71, hour_BCD); // hour
+
+            outb(0x70, 0x02);
+            outb(0x71, minute_BCD); // minute
+
+            outb(0x70, 0x00);
+            outb(0x71, second_BCD); // second
+
+            sys_req(WRITE, COM1, "\nTime set to: ", sizeof("\nTime set to: "));
+            get_time();
+        }
+
+        // re-enable interrupts
+        sti();
+
+        return 0;
+}
+
+int get_date(void){
+
+        //disabling the NMI bit
+        outb(0x70, inb(0x70) | 0x80);  
+        char str[100];
+        char *date_ptr;
+
+        //creating a slash char to put between the months/days/years
+        char slash[1];
+        slash[0] = '/';
+        sys_req(WRITE, COM1, "\n", sizeof("\n"));
+
+        //accessing the month bit
+        outb(0x70, 0x08);      
+
+            //changing the bit from bcd to a char ptr                      
+            date_ptr = (itoa(bcdToChar(inb(0x71)) - 48, str, 10));  
+
+            // write the month to the terminal (size checking)
+            if (atoi(date_ptr) <= 9) {
+                sys_req(WRITE, COM1, date_ptr, 1);
+            }
+            else {
+                sys_req(WRITE, COM1, date_ptr, sizeof(date_ptr));   
+            }
+
+             //writing the month and a slash to the terminal      
+            sys_req(WRITE, COM1, slash, sizeof(slash));     
+
+        //accessing the day bit
+        outb(0x70, 0x07);
+
+            //changing the bit from bcd to a char ptr                             
+            date_ptr = (itoa(bcdToChar(inb(0x71)) - 48, str, 10));
+
+            // write the day to the terminal (size checking)
+            if (atoi(date_ptr) <= 9) {
+                sys_req(WRITE, COM1, date_ptr, 1);
+            }
+            else {
+                sys_req(WRITE, COM1, date_ptr, sizeof(date_ptr));   
+            }
+            
+            // write a slash to the terminal
+            sys_req(WRITE, COM1, slash, sizeof(slash));
+
+        //accessing the year bit
+        outb(0x70, 0x09);  
+
+            //changing the bit from bcd to a char ptr                             
+            date_ptr = (itoa(bcdToChar(inb(0x71)) - 48, str, 10));
+
+            //writing the year to the terminal
+            sys_req(WRITE, COM1, date_ptr, sizeof(date_ptr));
+
+    //returning 0 indicates success   
+    return 0;
+}
+
+int set_date(char buf[]) {
+    
         // disable interrupts
         cli();
 
@@ -154,200 +350,6 @@ int comexec(char buf[]) {
 
         // re-enable interrupts
         sti();
-
-        return 0;
-    }
-    }
-
-    // "get_time" logic
-    else if (strcmp(buf, "get_time") == 0 || strcmp(buf, "gt") == 0) {
-        get_time();
-        return 0;
-    }
-
-    else if (strcmp(buf, "set_time") == 0) {
-        sys_req(WRITE, COM1, "\nPlease enter the time to be set (HH:MM): ", sizeof("\nPlease enter the time to be set (HH:MM): "));
-    }
-
-    // "set_time" logic
-    else if (contains(buf, "set_time") == 1) {
-
-       // disable interrupts
-        cli();
-
-        // derive time string from the buffer
-        char* time = substr(buf, 9);
-
-        // check for invalid format (invalid time check pending)
-        if (charCount(time, ':') != 2 || strlen(time) != 8 || isNumeric(strtok(time, ":")) == 0) {
-            sys_req(WRITE, COM1, "\nInvalid time or format. (HH:MM:SS)", sizeof("\nInvalid time or format. (HH:MM:SS)"));
-            return -1;
         }
-        else {
-            char hr_chars[3] = {time[0], time[1], '\0'}; // first 2 chars = hour value
-            char min_chars[3] = {time[3], time[4], '\0'}; // next 2 chars (skipping ':') = minute value
-            char sec_chars[3] = {time[6], time[7], '\0'}; // last 2 chars = second value
-
-            // check for invalid times / characters 
-            if (strcmp(hr_chars, "23") > 0 || strcmp(hr_chars, "0") < 0   
-            || strcmp(min_chars, "59") > 0 || strcmp(min_chars, "0") < 0  
-            || strcmp(sec_chars, "59") > 0 || strcmp(sec_chars, "0") < 0) 
-            {
-                sys_req(WRITE, COM1, "\nInvalid time or format. (HH:MM:SS)", sizeof("\nInvalid time or format. (HH:MM:SS)"));
-                return -1;
-            }
-
-            // retrieve int values for hour, minute, and second
-            unsigned int hour = (unsigned int)atoi(hr_chars) + 5; // +5 offset
-            unsigned int minute = (unsigned int)atoi(min_chars);
-            unsigned int second = (unsigned int)atoi(sec_chars);
-
-            // convert each time value to BCD
-            unsigned int hour_BCD = intToBCD(hour);
-            unsigned int minute_BCD = intToBCD(minute);
-            unsigned int second_BCD = intToBCD(second);
-
-            // write each BCD value to the corresponding register
-            outb(0x70, 0x04);
-            outb(0x71, hour_BCD); // hour
-
-            outb(0x70, 0x02);
-            outb(0x71, minute_BCD); // minute
-
-            outb(0x70, 0x00);
-            outb(0x71, second_BCD); // second
-
-            sys_req(WRITE, COM1, "\nTime set to: ", sizeof("\nTime set to: "));
-            get_time();
-        }
-
-        // re-enable interrupts
-        sti();
-
-        return 0;
-    }
-    if (strcmp(buf, "help") == 0) {
-        // Print list of commands
-        sys_req(WRITE, COM1, "\n@ help\t\tPrints a complete list of each available command.", sizeof("@ help\tPrints a complete list of each available command."));
-        sys_req(WRITE, COM1, "\n@ version\tPrints the current version of the program.", sizeof("@ version\tPrints the current version of the program."));
-        sys_req(WRITE, COM1, "\n@ get_date\tPrints the current date set by the user.", sizeof("@ get_date\tPrints the current date set by the user."));
-        sys_req(WRITE, COM1, "\n@ set_date\tSets the current date.", sizeof("@ set_date\tSets the current date."));
-        sys_req(WRITE, COM1, "\n@ get_time\tPrints the current time set by the user.", sizeof("@ get_time\tPrints the current time set by the user."));
-        sys_req(WRITE, COM1, "\n@ set_time\tSets the current time.", sizeof("@ set_time\tSets the current time."));
-        sys_req(WRITE, COM1, "\n@ shutdown\tExits the program.", sizeof("\n@ shutdown\tExits the program."));
-        return 0;
-    } 
-    else {
-        sys_req(WRITE, COM1, "\nError: Invalid command.", sizeof("\nError: Invalid command."));
-        return -1;
-    }
-}
-
-
-// I created this function for debugging purposes. We could give each command its own function if we want. - Chris
-int get_time() {
-    //disabling the NMI bit
-        outb(0x70, inb(0x70) | 0x80);
-        char str[100];
-        char *time_ptr;
-        char colon[1];
-        colon[0] = ':';
-        sys_req(WRITE, COM1, "\n", sizeof("\n"));
-
-        //accessing the hour bit
-        outb(0x70, 0x04);
-
-            //changing the hour bit from bcd to a char ptr
-            time_ptr = (itoa(bcdToChar(inb(0x71)) - 53, str, 10)); // -5 offset
-
-            //writing the hour bit and a colon to the terminal
-            sys_req(WRITE, COM1, time_ptr, sizeof(time_ptr));
-            sys_req(WRITE, COM1, colon, sizeof(colon));
-
-        // accessing the minute bit
-        outb(0x70, 0x02);
-
-            //changing the minute bit from bcd to a char ptr
-            time_ptr = (itoa(bcdToChar(inb(0x71)) - 48, str, 10));
-
-            // append 0 to avoid (HH:M:SS) error
-            if (atoi(time_ptr) <= 9) {
-                sys_req(WRITE, COM1, "0", sizeof("0"));
-            }
-
-            sys_req(WRITE, COM1, time_ptr, sizeof(time_ptr));
-            sys_req(WRITE, COM1, colon, sizeof(colon));
-
-        // accessing the second bit 
-        outb(0x70, 0x00);
-
-            //changing the second bit from bcd to a char ptr
-            int seconds = bcdToChar(inb(0x71)) - 48;
-            if(seconds < 10){
-                sys_req(WRITE, COM1, "0", sizeof("0"));
-            }
-            time_ptr = (itoa(seconds, str, 10));
-            sys_req(WRITE, COM1, time_ptr, sizeof(time_ptr));
-
-        // return 0 indicating success
-        return 0;
-}
-
-int get_date(){
-
-        //disabling the NMI bit
-        outb(0x70, inb(0x70) | 0x80);  
-        char str[100];
-        char *date_ptr;
-
-        //creating a slash char to put between the months/days/years
-        char slash[1];
-        slash[0] = '/';
-        sys_req(WRITE, COM1, "\n", sizeof("\n"));
-
-        //accessing the month bit
-        outb(0x70, 0x08);      
-
-            //changing the bit from bcd to a char ptr                      
-            date_ptr = (itoa(bcdToChar(inb(0x71)) - 48, str, 10));  
-
-            // write the month to the terminal (size checking)
-            if (atoi(date_ptr) <= 9) {
-                sys_req(WRITE, COM1, date_ptr, 1);
-            }
-            else {
-                sys_req(WRITE, COM1, date_ptr, sizeof(date_ptr));   
-            }
-
-             //writing the month and a slash to the terminal      
-            sys_req(WRITE, COM1, slash, sizeof(slash));     
-
-        //accessing the day bit
-        outb(0x70, 0x07);
-
-            //changing the bit from bcd to a char ptr                             
-            date_ptr = (itoa(bcdToChar(inb(0x71)) - 48, str, 10));
-
-            // write the day to the terminal (size checking)
-            if (atoi(date_ptr) <= 9) {
-                sys_req(WRITE, COM1, date_ptr, 1);
-            }
-            else {
-                sys_req(WRITE, COM1, date_ptr, sizeof(date_ptr));   
-            }
-            
-            // write a slash to the terminal
-            sys_req(WRITE, COM1, slash, sizeof(slash));
-
-        //accessing the year bit
-        outb(0x70, 0x09);  
-
-            //changing the bit from bcd to a char ptr                             
-            date_ptr = (itoa(bcdToChar(inb(0x71)) - 48, str, 10));
-
-            //writing the year to the terminal
-            sys_req(WRITE, COM1, date_ptr, sizeof(date_ptr));
-
-    //returning 0 indicates success   
     return 0;
 }
