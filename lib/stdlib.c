7234f8746fd43b3265f1d8b9e47e7a8ef407ce6f
TunarForbes <tannerf0526@gmail.com>
2025-02-05 14:18:08 -0500

    add comments to bcdToChar and itoa functions in stdlib.c

--- lib/stdlib.c
+++ lib/stdlib.c
@@ -29,24 +29,35 @@ int atoi(const char *s)
 }
 
 char* itoa(int num, char* str, int base){
-	int i = 0;
+	
+	//Initilization of a int variable to keep track of the position within the char array
+	int pos = 0;
 
+	//checking to see if the num to convert is a 0
 	if(num == 0){
-		str[i++] = '0';
-		str[i] = '\0';
+		str[pos++] = '0';
+		str[pos] = '\0';
 		return str;
 	}
 
+	//looping while the num is not a 0
 	while(num != 0){
+
+		//Modding the num by the base in order to get the remainder
 		int rem = num % base;
-		str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
+		str[pos++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
+
+		//Dividing the number by the base to get rid of the least significant digit
 		num = num / base;
 	}
 
-	str[i] = '\0';
+	//Once out of the loop add a null terminator to the end of the char array
+	str[pos] = '\0';
 
 	int start = 0;
-	int end = i - 1;
+	int end = pos - 1;
+	
+	//inverting the char array to get the correct ASCII string
 	while(start < end) {
 		char temp = str[start];
 		str[start] = str[end];
@@ -55,6 +66,7 @@ char* itoa(int num, char* str, int base){
 		start++;
 	}
 
+	//returning the passed str parameter
 	return str;
 
 	
@@ -62,7 +74,11 @@ char* itoa(int num, char* str, int base){
 
 // Implementation for bcdToChar
 char bcdToChar(unsigned char bcd){
+	
+	//converting the given bcd to decimal by bit shifting then multiplying then adding
     int decimal = (bcd >> 4) * 10 + (bcd & 0x0F);
+
+	//returning the int plus a char to turn the given int into a char
     return decimal + '0';
 }
 
