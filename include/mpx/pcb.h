0c047fdd35aab918eae84500c66fd1a53a207ebc
caj00017 <caj00017@CJLaptop>
2025-02-10 14:01:11 -0500

    setters

--- include/mpx/pcb.h
+++ include/mpx/pcb.h
@@ -29,12 +29,30 @@ typedef struct {
 char* get_name(pcb* pcb);
 
 /**
+ * @brief Set the name of the pcb
+ * @param pcb* pcb The pcb to set the name of
+ * @param char* name The name to set
+ * @returns Status code
+ * @author Chris Jones
+ */
+int set_name(pcb* pcb, char* name);
+
+/**
  * @brief Get the class of the pcb
  * @param pcb* pcb The pcb to get the class of
  * @returns int The class of the pcb (0 - user process, 1 - kernel process)
  * @author Chris Jones
  */
 int get_class(pcb* pcb);
+
+/**
+ * @brief Set the class of the pcb
+ * @param pcb* pcb The pcb to set the class of
+ * @param int class The class to set
+ * @returns Status code
+ * @author Chris Jones
+ */
+int set_class(pcb* pcb, int class);
 
 /**
  * @brief Get the priority of the pcb
@@ -45,12 +63,30 @@ int get_class(pcb* pcb);
 int get_priority(pcb* pcb);
 
 /**
+ * @brief Set the priority of the pcb
+ * @param pcb* pcb The pcb to set the priority of
+ * @param int priority The priority to set
+ * @returns Status code
+ * @author Chris Jones
+ */
+int set_priority(pcb* pcb, int priority);
+
+/**
  * @brief Get the state of the pcb
  * @param pcb* pcb The pcb to get the state of
  * @returns int The state of the pcb (0-7)
  * @author Chris Jones
  */
 int get_state(pcb* pcb);
+
+/**
+ * @brief Set the state of the pcb
+ * @param pcb* pcb The pcb to set the state of
+ * @param int state The state to set
+ * @returns Status code
+ * @author Chris Jones
+ */
+int set_state(pcb* pcb, int state);
 
 /**
  * @brief Get the execution mode of the pcb
@@ -61,12 +97,30 @@ int get_state(pcb* pcb);
 int get_execution_mode(pcb* pcb);
 
 /**
+ * @brief Set the execution mode of the pcb
+ * @param pcb* pcb The pcb to set the execution mode of
+ * @param int execution_mode The execution mode to set
+ * @returns Status code
+ * @author Chris Jones
+ */
+int set_execution_mode(pcb* pcb, int execution_mode);
+
+/**
  * @brief Get the name of the pcb
  * @param pcb* pcb The pcb to get the name of
  * @returns int The dispatch mode of the pcb
  * @author Chris Jones
  */
 int get_dispatch_mode(pcb* pcb);
+
+/**
+ * @brief Set the dispatch mode of the pcb
+ * @param pcb* pcb The pcb to set the dispatch mode of
+ * @param int dispatch_mode The dispatch mode to set
+ * @returns Status code
+ * @author Chris Jones
+ */
+int set_dispatch_mode(pcb* pcb, int dispatch_mode);
 
 /**
  * @brief Get the stack location of the pcb
@@ -77,6 +131,14 @@ int get_dispatch_mode(pcb* pcb);
 void* get_stack_location(pcb* pcb);
 
 /**
+ * @brief Set the stack location of the pcb
+ * @param pcb* pcb The pcb to set the stack location of
+ * @param void* stack_location The stack location to set
+ * @returns Status code
+ */
+int set_stack_location(pcb* pcb, void* stack_location);
+
+/**
  * @brief Get the next node in the pcb queue
  * @param pcb* pcb The current pcb
  * @returns pcb* The next node in the pcb queue
@@ -85,11 +147,29 @@ void* get_stack_location(pcb* pcb);
 pcb* get_next_node(pcb* pcb);
 
 /**
+ * @brief Set the next node in the pcb queue
+ * @param pcb* pcb The current pcb
+ * @param pcb* next_node The next node in the pcb queue
+ * @returns int Status code
+ * @author Chris Jones
+ */
+int set_next_node(pcb* pcb, pcb* next_node);
+
+/**
  * @brief Get the previous node in the pcb queue
  * @param pcb* pcb The current pcb
  * @returns pcb* The previous node in the pcb queue
  * @author Chris Jones
  */
 pcb* get_prev_node(pcb* pcb);
+
+/**
+ * @brief Set the previous node in the pcb queue
+ * @param pcb* pcb The current pcb
+ * @param pcb* prev_node The previous node in the pcb queue
+ * @returns int Status code
+ * @author Chris Jones
+ */
+int set_prev_node(pcb* pcb, pcb* prev_node);
 
 #endif
--- kernel/pcb.c
+++ kernel/pcb.c
@@ -10,35 +10,80 @@ char* get_name(pcb* pcb) {
     return pcb.name;
 }
 
+int set_name(pcb* pcb, char* name) {
+    pcb.name = name;
+    return 0;
+}
+
 int get_class(pcb* pcb) {
     return pcb.class;
+}
+
+int set_class(pcb* pcb, int class) {
+    pcb.class = class;
+    return 0;
 }
 
 int get_priority(pcb* pcb) {
     return pcb.priority;
 }
 
+int set_priority(pcb* pcb, int priority) {
+    pcb.priority = priority;
+    return 0;
+}
+
 int get_state(pcb* pcb) {
     return pcb.state;
+}
+
+int set_state(pcb* pcb, int state) {
+    pcb.state = state;
+    return 0;
 }
 
 int get_execution_mode(pcb* pcb) {
     return pcb.execution_mode;
 }
 
+int set_execution_mode(pcb* pcb, int execution_mode) {
+    pcb.execution_mode = execution_mode;
+    return 0;
+}
+
 int get_dispatch_mode(pcb* pcb) {
     return pcb.dispatch_mode;
+}
+
+int set_dispatch_mode(pcb* pcb, int dispatch_mode) {
+    pcb.dispatch_mode = dispatch_mode;
+    return 0;
 }
 
 int get_stack_location(pcb* pcb) {
     return pcb.stack_location;
 }
 
+int set_stack_location(pcb* pcb, void* stack_location) {
+    pcb.stack_location = stack_location;
+    return 0;
+}
+
 pcb* get_next_node(pcb* pcb) {
     return pcb.next_node;
 }
 
+int set_next_node(pcb* pcb, pcb* next_node) {
+    pcb.next_node = next_node;
+    return 0;
+}
+
 pcb* get_prev_node(pcb* pcb) {
     return pcb.prev_node;
+}
+
+int set_prev_node(pcb* pcb, pcb* prev_node) {
+    pcb.prev_node = prev_node;
+    return 0;
 }
 
