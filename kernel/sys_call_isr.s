f88e0860f0271e701bbba1cf8442bcd892d4786d
TunarForbes <tannerf0526@gmail.com>
2025-03-10 21:28:38 -0400

    all changes in sys_call_isr

--- kernel/sys_call_isr.s
+++ kernel/sys_call_isr.s
@@ -1,40 +1,38 @@
 bits 32
 global sys_call_isr
+extern sys_call			; The C function that sys_call_isr will call
 
 ;;; System call interrupt handler. To be implemented in Module R3.
 
-extern sys_call			; The C function that sys_call_isr will call
+section .text
 sys_call_isr:
-	mov eax, -1		;; DELETE THIS LINE WHEN YOU IMPLEMENT R3
-	iret
+	push EBP  ;pushing all registers onto stack
+	push EDI
+	push ESI
+	push EDX
+	push ECX
+	push EBX
+	push EAX
+	push SS
+	push ES
+	push DS
+	push FS
+	push GS
+	
+	push ESP
+	call sys_call
+	mov EAX, ESP
 
-; extern sys_call			; The C function that sys_call_isr will call
-; sys_call_isr:
-; 	push ESP
-; 	push EBP
-; 	push EDI
-; 	push EDX
-; 	push ECX
-; 	push EBX
-; 	push EAX
-; 	push SS
-; 	push GS
-;	push FS	
-;	push ES
-;	push DS
-;	call sys_call
-;	mov ESP, EAX
-;	pop DS
-;	pop ES
-;	pop FS
-;	pop GS
-;	pop SS
-;	pop EAX
-;	pop EBX
-;	pop ECX
-;	pop EDX
-;	pop ESI
-;	pop EDI
-;	pop EBP
-;	pop ESP
-;	iret
+	pop GS
+	pop FS
+	pop DS
+	pop ES
+	pop SS
+	pop EAX
+	pop EBX
+	pop ECX
+	pop EDX
+	pop ESI
+	pop EDI
+	pop EBP
+	iret
