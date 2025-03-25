ebac95e9af9a1160bca8781b17b72df6d40f5d9c
IzaakWhetsell <izaakwhetsell@gmail.com>
2025-03-25 17:17:06 -0400

    Updated version for R3 - 3/28/2025

--- user/comexec.c
+++ user/comexec.c
@@ -911,7 +911,7 @@ int help(void) {
 
 int version(void) {
     println();
-    print("Version 2.0\nCompilation Date: 2/28/2025");
+    print("Version 3.0\nCompilation Date: 3/28/2025");
     return 0;
 }
 
