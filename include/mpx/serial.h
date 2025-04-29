c277039abfb73c30d9fe1d2ed6e2093361f80141
emh00015 <emh00015@github.com>
2025-04-29 12:17:39 -0400

    added a new field for DCB to include device address

--- include/mpx/serial.h
+++ include/mpx/serial.h
@@ -7,7 +7,8 @@
 #include <mpx/device.h>
 
 typedef struct dcb {
-    int dev;    
+    int dev;
+    device dev_address; 
     int open; // 0 closed, 1 open
     int status; // 0 idle, 1 reading, 2 writing
     int* event_flag; // for i/o completion signaling (0 = start, 1 = complete)
